029b3aadc65da013b0604f73a12fa203beace5f8
Kalyn Simms <Kalynbsimms@gmail.com>
2020-08-16 20:15:28 -0700

    added manager

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+const render = require("./lib/htmlRenderer");
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+let employees = []
+
+const buildManager = employee => {
+   inquirer.prompt ([
+       {
+           type: 'number',
+           name: 'officeNumber',
+           message: `What is the manager's office number?`
+       }
+   ]) 
+
+   .then(({officeNumber}) => {
+       employees.push(new Manager(employee.name,employee.id,employee.email,officeNumber))
+       subMenu()
+   })
+   .catch(err => console.log(err))
+}
+
+
+const buildEngineer = employee => {
+ inquirer.prompt ([
+     {
+         type: 'input',
+         name: ''
+     }
+ ])
+
+}
+
+
+
+
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
