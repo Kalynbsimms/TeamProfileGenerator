d9127990eca81ef402150b697d77c62065d9ab8b
Kalyn Simms <Kalynbsimms@gmail.com>
2020-08-16 21:49:15 -0700

    app js code

--- app.js
+++ app.js
@@ -14,77 +14,127 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 let employees = []
 
 const buildManager = employee => {
-   inquirer.prompt ([
-       {
-           type: 'number',
-           name: 'officeNumber',
-           message: `What is the manager's office number?`
-       }
-   ]) 
+    inquirer.prompt([
+        {
+            type: 'number',
+            name: 'officeNumber',
+            message: `What is the manager's office number?`
+        }
+    ])
 
-   .then(({officeNumber}) => {
-       employees.push(new Manager(employee.name,employee.id,employee.email,officeNumber))
-       subMenu()
-   })
-   .catch(err => console.log(err))
+        .then(({ officeNumber }) => {
+            employees.push(new Manager(employee.name, employee.id, employee.email, officeNumber))
+            subMenu()
+        })
+        .catch(err => console.log(err))
 }
 
 
 const buildEngineer = employee => {
- inquirer.prompt ([
-     {
-         type: 'input',
-         name: 'gitHub',
-         message: 'What is your github username?'
-     }
- ])
-.then(({github})=> {
-    employees.push(new Engineer(employee.name,employee.id,employee.email,gitHub))
-    subMenu()
-})
-.catch(err => console.log(err))
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: 'What is your github username?'
+        }
+    ])
+        .then(({ github }) => {
+            employees.push(new Engineer(employee.name, employee.id, employee.email, gitHub))
+            subMenu()
+        })
+        .catch(err => console.log(err))
 }
 
 const buildIntern = employee => {
     inquirer.prompt([
         {
-            type:'input',
+            type: 'input',
             name: 'school',
             message: 'What school do you attend?'
         }
     ])
-    .then (({school}) => {
-        employees.push(new Intern(employee.name,employee.id,employee.email,school))
-        subMenu()
-    })
-    .catch(err => console.log(err))
-    
+        .then(({ school }) => {
+            employees.push(new Intern(employee.name, employee.id, employee.email, school))
+            subMenu()
+        })
+        .catch(err => console.log(err))
+
 }
 
 const subMenu = () => {
     inquirer.prompt({
         type: 'list',
         name: 'action',
-        choice : ['Make Another Employee','Finish'],
+        choice: ['Make Another Employee', 'Finish'],
         message: 'What would you like to do now?'
     })
-    .then(({ action }) => {
-        switch (action) {
-        case 'Make Another Employee':
-        mainMenu()
-        break
-        case 'Finish':
-        const html = render (employees)
-                fs.writeFileSync(path.join(__dirname, 'output', 'main.html'), html)
-                break
+        .then(({ action }) => {
+            switch (action) {
+                case 'Make Another Employee':
+                    mainMenu()
+                    break
+                case 'Finish':
+                    const html = render(employees)
+                    fs.writeFileSync(path.join(__dirname, 'output',
+                    // Should this be main.html or index.html
+                     'main.html'), html)
+                    break
 
+            }
+        })
+        .catch(err => console.log(err))
+
+
+}
+
+const mainMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'type',
+            choices: ['Employee', 'Manager', 'Engineer', 'Intern'],
+            message: 'Select the type of employee you would like to make'
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter employee name:'
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: 'Enter employee ID number:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter employee email address:'
         }
-       })
-       .catch(err => console.log(err))
+    ])
+        .then(employee => {
+            switch (employee.type) {
+                case 'Employee':
+                    employees.push(new Employee(employee.name, employee.id, employee.email))
+                    subMenu()
+                    break
+                case 'Manager':
+                    buildManager(employee)
+                    break
+                case 'Engineer':
+                    buildEngineer(employee)
+                    break
+                case 'Intern':
+                    buildIntern(employee)
+                    break
 
+            }
+        })
 
-        }
-    
+        .catch(err => console.log(err))
+}
+
+mainMenu()
+
 
 
 
